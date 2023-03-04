4b731c854b21604ac7c12635eed39faca12f9237
MyPC <MyPC@DESKTOP-CITU7RE>
2023-03-04 12:06:38 +0100

    Inizio Programma triangolo 04/03/2023

---
+++ src/it/poligono/Triangolo.java
@@ -0,0 +1,33 @@
+package it.poligono;
+
+public class Triangolo {
+	private float base;
+	private float altezza;
+	
+	public Triangolo() {	
+	}
+
+	public float getBase() {
+		return base;
+	}
+
+	public void setBase(float base) {
+		this.base = base;
+	}
+
+	public float getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(float altezza) {
+		this.altezza = altezza;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangolo [base=" + base + ", altezza=" + altezza + "]";
+	}
+	
+	
+
+}
